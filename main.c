0983154cf9a21eab798a16da8d30e61f573a6775
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-11-20 20:07:32 +0545

    encrpytion done

--- main.c
+++ main.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define True 1
-#define False 0
-
 void show_usages(void);
-
+FILE *read_file(char *,char *);
+void encrypt(char *,char *);
 
 void show_usages(void){
 	printf("\n\tascii-encryptor\tInvalid Usage\n\t\tUsage:\tascii-encrypt <fileToEncrypt> <OutputFile> <en/de>\n");
@@ -29,20 +27,33 @@ FILE *read_file(char *file,char *mode)
 
 void encrypt(char *master, char *slave)
 {
-	printf("ENCRYPTING");
+	FILE *source = read_file(master,"r");
+	FILE *destination = read_file(slave,"w");
+
+
+	char buf;
+
+	// read byte by byte (char by char) and write it to the new file 
+	while (((buf = getc(source))!= EOF)){
+		fprintf(destination,"%d ",buf);
+	}
+	
+	// close the files 
+	fclose(source);
+	fclose(destination);
+
+	printf("\nSucessfully Encrypted\n");
 }
 
 void main(int argc, char *argv[]){
 
 	// check if correct arguments are supplied
-	if (argc < 4){
+	if (argc < 4)
 		show_usages();
-	}
 	
-	if ((strcmp(argv[3],"en")) == 0) {
+	if ((strcmp(argv[3],"en")) == 0) 
 		encrypt(argv[1],argv[2]);			
-	}
-
+	
 	else if((strcmp(argv[3],"de")) == 0){
 	
 	}
@@ -51,24 +62,6 @@ void main(int argc, char *argv[]){
 		show_usages();
 	}
 
-	FILE *fp = fopen(argv[1],"r"); // reading file
-	FILE *fpw = fopen(argv[2],"w");  // writing file
-	char buf;
-
-	// if error
-	if (fp == NULL || fpw == NULL){
-		printf("There has been some error while opening the file. Does that file exist?\n\n");
-		exit(-1);
-	}
-	
-
-	// read byte by byte (char by char) and write it to the new file 
-	while ((buf = getc(fp))!= EOF){
-		fprintf(fpw,"%d ",buf);
-	}
-	
-	// close the files
-	fclose(fp);
-	fclose(fpw);
+	char buf;	
 
 }
