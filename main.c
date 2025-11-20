baf5966b1cc2d18ab8864cdba45cdfe5f284a0b6
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-11-20 19:45:47 +0545

    correct checking of inputs is done

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define True 1
+#define False 0
 
 void show_usages(void);
 
 
 void show_usages(void){
-	printf("\n\tascii-encryptor\tInvalid Usage\n\t\tUsage:\tascii-encrypt <fileToEncrypt> <OutputFile>\n");
+	printf("\n\tascii-encryptor\tInvalid Usage\n\t\tUsage:\tascii-encrypt <fileToEncrypt> <OutputFile> <en/de>\n");
 }
 
 void main(int argc, char *argv[]){
 
 	// check if correct arguments are supplied
-	if (argc < 3){
+	if (argc < 4){
 		show_usages();
 		exit(0); // quit program
 	}
+	
+	if (strcmp(argv[3],"en")) == 0 {
+			
+	}
 
-	FILE *fp = fopen("test.txt","r"); // reading file
-	FILE *fpw = fopen("test2.txt","w");  // writing file
+	else if(strcmp(argv[3],"de")) == 0{
+	
+	}
+
+	else{
+		show_usages();
+		exit(0);
+	}
+
+	FILE *fp = fopen(argv[1],"r"); // reading file
+	FILE *fpw = fopen(argv[2],"w");  // writing file
 	char buf;
 
 	// if error
@@ -26,6 +43,10 @@ void main(int argc, char *argv[]){
 		exit(-1);
 	}
 	
+	// find out if it's encrypting or decrypting
+	switch (strcmp(argv[3],"enc"))
+	{
+
 	// read byte by byte (char by char) and write it to the new file 
 	while ((buf = getc(fp))!= EOF){
 		fprintf(fpw,"%d ",buf);
